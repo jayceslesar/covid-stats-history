dcb08981c3db2365000f5035ce069dc953e5ec0c
Jayce Slesar <47452474+jayceslesar@users.noreply.github.com>
2020-05-31 14:16:57 -0400

    set up for linux

--- text_funcs.py
+++ text_funcs.py
@@ -15,25 +15,27 @@ def get_text(DOI:str) -> str:
     url = "https://www.medrxiv.org/content/" + DOI + "v1.full.pdf"  # build url
     response = requests.get(url)
     fp.write_bytes(response.content)  # save .pdf
+
+    if sys.platform.startswith('linux'):
+        pass
     
-    word = win32com.client.Dispatch("Word.Application")
-    word.visible = 0  # supress errors in console (probably get a lot of errors from images we don't care about)
-    pdfs_path = "pdfs/" # folder where the .pdf files are stored
-    for i, doc in enumerate(glob.iglob(pdfs_path + "*.pdf")):
-        filename = doc.split('\\')[-1]
-        in_file = os.path.abspath(doc)
-        wb = word.Documents.Open(in_file)
-        out_file = os.path.abspath(Path(Path.cwd() / "pdfs" / "curr.docx"))
-        wb.SaveAs2(out_file, FileFormat=16) # file format for docx
-        wb.Close()
-    word.Quit()
-
-
-    doc = docx.Document(Path(Path.cwd() / "pdfs" / "curr.docx"))  # build path
-    full_text = []  # store text
-    for para in doc.paragraphs:
-        full_text.append(para.text.encode(sys.stdout.encoding, errors='replace'))  # fix encodings and append
-    return b'\n'.join(full_text).decode('windows-1252')  # one final decode -> will look different on linux
+    else:
+        word = win32com.client.Dispatch("Word.Application")
+        word.visible = 0  # supress errors in console (probably get a lot of errors from images we don't care about)
+        pdfs_path = "pdfs/" # folder where the .pdf files are stored
+        for i, doc in enumerate(glob.iglob(pdfs_path + "*.pdf")):
+            filename = doc.split('\\')[-1]
+            in_file = os.path.abspath(doc)
+            wb = word.Documents.Open(in_file)
+            out_file = os.path.abspath(Path(Path.cwd() / "pdfs" / "curr.docx"))
+            wb.SaveAs2(out_file, FileFormat=16) # file format for docx
+            wb.Close()
+        word.Quit()
+        doc = docx.Document(Path(Path.cwd() / "pdfs" / "curr.docx"))  # build path
+        full_text = []  # store text
+        for para in doc.paragraphs:
+            full_text.append(para.text.encode(sys.stdout.encoding, errors='replace'))  # fix encodings and append
+        return b'\n'.join(full_text).decode('windows-1252')  # one final decode -> will look different on linux
 
 
 # DOI = "10.1101/2020.04.08.20058487"
