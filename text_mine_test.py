d8797e6083a745997d511703db045b2251513037
Jayce Slesar <47452474+jayceslesar@users.noreply.github.com>
2020-05-31 19:27:50 -0400

    added logs to separate warnings from data

--- text_mine_test.py
+++ text_mine_test.py
@@ -5,6 +5,7 @@ import warnings
 
 warnings.filterwarnings("ignore")
 
+log = ""
 
 # "reproductive number", "Basic reproductive number"
 # "Latent period prior to infectivity"
@@ -16,7 +17,9 @@ for index, row in df.iterrows():
     try:
         text = text_funcs.get_text(row["DOI"])
         if "R0" not in text:  # if it isn't there skip
+            row["flag"] = "bad"
             continue
+        log += row["title"] + '\n'
         print(row["title"])
         if first in text:  # TODO::make regex searches and findall's
             print("R0 found...")
