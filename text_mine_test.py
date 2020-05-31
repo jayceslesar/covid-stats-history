0846911e8117978462a549d140d9b28650095cd8
Jayce Slesar <47452474+jayceslesar@users.noreply.github.com>
2020-05-31 14:17:21 -0400

    tried to speed up, more to come

--- text_mine_test.py
+++ text_mine_test.py
@@ -2,7 +2,9 @@ import pandas as pd
 import text_funcs
 
 
-
+# "reproductive number", "Basic reproductive number"
+# "Latent period prior to infectivity"
+# "Duration of infectivity"
 first = "R0="
 second = "R0 ="
 df = pd.read_csv("rxiv.csv")
@@ -10,15 +12,16 @@ for index, row in df.iterrows():
     try:
         print(row["title"])
         text = text_funcs.get_text(row["DOI"])
-        print("R0" in text)
-        if first in text:
+        if "R0" not in text:  # if it isn't there skip
+            continue
+        if first in text:  # seaches -- need to be better
             print("R0 found...")
             i = text.index(first)
-            print("R0", text[i:i + len(first) + 3])
+            print(text[i:i + len(first) + 3])
         if second in text:
             print("R0 found...")
             i = text.index(second)
-            print("R0", text[i:i + len(second) + 3])
+            print(text[i:i + len(second) + 3])
         else:
             print("Not found")
     except Exception as e:
