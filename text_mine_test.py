edc08681ab35c7b3c0ff80f40fd4d9ee56e0d457
Jayce Slesar <47452474+jayceslesar@users.noreply.github.com>
2020-05-31 17:59:24 -0400

    updated with new method

--- text_mine_test.py
+++ text_mine_test.py
@@ -1,5 +1,9 @@
 import pandas as pd
 import text_funcs
+import warnings
+
+
+warnings.filterwarnings("ignore")
 
 
 # "reproductive number", "Basic reproductive number"
@@ -10,21 +14,21 @@ second = "R0 ="
 df = pd.read_csv("rxiv.csv")
 for index, row in df.iterrows():
     try:
-        print(row["title"])
         text = text_funcs.get_text(row["DOI"])
         if "R0" not in text:  # if it isn't there skip
             continue
-        if first in text:  # seaches -- need to be better
+        print(row["title"])
+        if first in text:  # TODO::make regex searches and findall's
             print("R0 found...")
             i = text.index(first)
-            print(text[i:i + len(first) + 3])
+            print(text[i:i + len(first) + 8])
         if second in text:
             print("R0 found...")
             i = text.index(second)
-            print(text[i:i + len(second) + 3])
+            print(text[i:i + len(second) + 8])
         else:
             print("Not found")
     except Exception as e:
-        print(e)
+        print("EXCEPTION", e)
         pass
     print("------------------------------------------------------------------------------------------------------------------")
