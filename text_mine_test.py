c02d7cf69c69040946a5553d20dde298782f8528
Jayce Slesar <47452474+jayceslesar@users.noreply.github.com>
2020-06-04 10:29:08 -0400

    should work

--- text_mine_test.py
+++ text_mine_test.py
@@ -1,37 +1,62 @@
 import pandas as pd
 import text_funcs
 import warnings
+import re
+import json
 
 
 warnings.filterwarnings("ignore")
 
-log = ""
 
 # "reproductive number", "Basic reproductive number"
 # "Latent period prior to infectivity"
 # "Duration of infectivity"
-first = "R0="
-second = "R0 ="
+
+search_params = {"R0": ["R0=", "R0 =", "R0", "reproductive number"]}
+R0_LOWER_BOUND = 0.9
+R0_UPPER_BOUND = 6.5
+d = {}
+float_finder = re.compile(r"[-+]?\d*\.\d+|\d+")
+
 df = pd.read_csv("rxiv.csv")
 for index, row in df.iterrows():
     try:
+        print(index, row["title"])
         text = text_funcs.get_text(row["DOI"])
-        if "R0" not in text:  # if it isn't there skip
-            row["flag"] = "bad"
-            continue
-        log += row["title"] + '\n'
-        print(row["title"])
-        if first in text:  # TODO::make regex searches and findall's
-            print("R0 found...")
-            i = text.index(first)
-            print(text[i:i + len(first) + 8])
-        if second in text:
-            print("R0 found...")
-            i = text.index(second)
-            print(text[i:i + len(second) + 8])
-        else:
-            print("Not found")
+        for p in search_params.keys():
+            matches = []
+            if p in text:
+                for p_mod in search_params[p]:
+                    for match in re.finditer(p_mod, text):
+                        matches.append((text[match.start():match.end() + 20]))
+                new_matches = []
+                for m in matches:
+                    new_matches.append(re.findall(float_finder, m))
+                actual_matches = []
+                for ms in new_matches:
+                    if len(ms) == 0:
+                        continue
+                    for m in ms:
+                        if '.' in m and float(m) > R0_LOWER_BOUND and float(m) < R0_UPPER_BOUND:
+                            actual_matches.append(float(m))
+                if len(actual_matches) == 0:
+                    continue
+                print("R0 Found", actual_matches)
+            row["R0"] = actual_matches
+            d[row["DOI"]] = actual_matches
     except Exception as e:
         print("EXCEPTION", e)
         pass
-    print("------------------------------------------------------------------------------------------------------------------")
+    print("------------------------------------------------------------------------------------------------")
+
+try:
+    print(d)
+except:
+    try:
+        for paper in d.keys():
+            print(paper, d[paper])
+    except:
+        print(paper, "Failed")
+        pass
+
+df.to_csv("R0.csv")
