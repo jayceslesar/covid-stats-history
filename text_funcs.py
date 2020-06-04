7a230bb84fb68b873151d61c1a899eb67402a11a
Jayce Slesar <47452474+jayceslesar@users.noreply.github.com>
2020-06-04 10:29:02 -0400

    new pdf reader

--- text_funcs.py
+++ text_funcs.py
@@ -1,31 +1,19 @@
-import requests
+from tika import parser
+import pandas as pd
 from pathlib import Path
+import requests
 import sys
-import glob
-import win32com.client
-import os
-import pdfreader
-from pdfreader import PDFDocument, SimplePDFViewer
-from threading import Timer  # will use to determine if the thing is stuck on images or not hopefully
 
 
 def get_text(DOI:str) -> str:
-    txt = ""
     name = "curr."
     fp = Path(Path.cwd() / "pdfs" / "curr.pdf")  # build filepath
     url = "https://www.medrxiv.org/content/" + DOI + "v1.full.pdf"  # build url
     response = requests.get(url)
-    fp.write_bytes(response.content)  # save .pdf
+    fp.write_bytes(response.content)  # save .pdf  # BUG::writes encoded characters as bytes and reads them incorrectly !?!?!?
     
-    fd = open(r"pdfs\curr.pdf", "rb")  # open with pdfreader
-    doc = PDFDocument(fd)
-    all_pages = [p for p in doc.pages()]  # get pages
-    viewer = SimplePDFViewer(fd)  # use simple viwer
-
-    for p in range(len(all_pages)):  # for each page
-        viewer.navigate(p + 1)  # nav to page
-        viewer.render()  # render -? clean and strip
-        txt += (u"".join(viewer.canvas.strings).encode(sys.stdout.encoding, errors='replace').decode("windows-1252")) + '\n' 
+    raw = parser.from_file(r"pdfs\curr.pdf")
+    txt = raw['content'].encode().decode('unicode_escape')
 
     return txt
 
