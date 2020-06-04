c2acbc3b2ebebe1dd3ba63010f0c775118f5315f
Jayce Slesar <47452474+jayceslesar@users.noreply.github.com>
2020-06-04 14:37:38 -0400

    fixes

--- text_mine_test.py
+++ text_mine_test.py
@@ -1,12 +1,10 @@
 import pandas as pd
 import text_funcs
-import warnings
 import re
 import json
 
 
-warnings.filterwarnings("ignore")
-
+new = []
 
 # "reproductive number", "Basic reproductive number"
 # "Latent period prior to infectivity"
@@ -22,61 +20,65 @@ float_finder = re.compile(r"[-+]?\d*\.\d+|\d+")  # regex for float
 
 df = pd.read_csv("rxiv.csv")
 for index, row in df.iterrows():
+    run = {}
+    # store first gen matches (text)
+    matches = []
     try:
         print(index, row["title"])
         # get the text
         text = text_funcs.get_text(row["DOI"])
         # try each search param
         for p in search_params.keys():
-            # store first gen matches (text)
-            matches = []
             if p in text:
                 # each paramater permutation
                 for p_mod in search_params[p]:
                     # look for each permutation in text
                     for match in re.finditer(p_mod, text):
                         # grab the start of it to the end of it and then offset
-                        OFFSET = 20
+                        OFFSET = 15
                         potential = text[match.start():match.end() + OFFSET]
-                        # if the search query is at the end of a sentence ignore it
-                        if potential[potential.index(p_mod) + len(p_mod) + 1] == '.':
-                            continue
-                        # else it is a match
-                        matches.append(potential)
-                # store second gen matches (numbers)
-                new_matches = []
-                # strip numbers out of text
-                for m in matches:
-                    new_matches.append(re.findall(float_finder, m))
-                # store third gen matches (filtered numbers)
-                actual_matches = []
-                # filter the numbers by bound and make sure they are a float
-                for ms in new_matches:
-                    if len(ms) > 0:
-                        for m in ms:
-                            if '.' in m and float(m) > R0_LOWER_BOUND and float(m) < R0_UPPER_BOUND:
-                                actual_matches.append(float(m))
-                # remove dupes anmd turn into list
-                actual_matches = list(set(actual_matches))
-                # skip if the size of the list iz zero
-                if len(actual_matches) == 0:
-                    continue
-                print("R0 Found", actual_matches)
-            row["R0"] = actual_matches
+                        # if the search query is not at the end of a sentence
+                        if potential[potential.index(p_mod) + len(p_mod) + 1] != '.':
+                            # grab text
+                            matches.append(potential)
+                        
+        # store second gen matches (numbers)
+        new_matches = []
+        # strip numbers out of text
+        for m in matches:
+            new_matches.append(re.findall(float_finder, m))
+        # store third gen matches (filtered numbers)
+        actual_matches = []
+        # filter the numbers by bound and make sure they are a float
+        for ms in new_matches:
+            if len(ms) > 0:
+                for m in ms:
+                    if '.' in m and float(m) > R0_LOWER_BOUND and float(m) < R0_UPPER_BOUND:
+                        actual_matches.append(float(m))
+        # remove dupes and turn into list
+        actual_matches = list(set(actual_matches))
+        if len(actual_matches) != 0:
+            print("R0 Found", actual_matches)
+            run["title"] = row["title"]
+            run["DOI"] = row["DOI"]
+            run["abstract"] = row["abstract"]
+            run["pre_print_release_date"] = row["pre_print_release_date"]
+            run["publisher"] = row["publisher"]
+            run["authored_by"] = row["authored_by"]
+            run["R0"] = actual_matches
+            # fill in others...
+            run["database"] = row["database"]
+            run["flag"] = ""
+            new.append(run)
             d[row["DOI"]] = actual_matches
+
     except Exception as e:
         print("EXCEPTION", e)
         pass
     print("------------------------------------------------------------------------------------------------")
 
-try:
-    print(d)
-except:
-    try:
-        for paper in d.keys():
-            print(paper, d[paper])
-    except:
-        print(paper, "Failed")
-        pass
 
+new = pd.DataFrame(d)
+new.to_csv("R0test.csv")
+print(d)
 df.to_csv("R0.csv")
