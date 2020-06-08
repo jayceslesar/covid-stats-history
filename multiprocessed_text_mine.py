73c1b43af985b45f1998e660563f473c1c2c3047
Jayce Slesar <47452474+jayceslesar@users.noreply.github.com>
2020-06-08 16:33:08 -0400

    ?

--- multiprocessed_text_mine.py
+++ multiprocessed_text_mine.py
@@ -12,6 +12,8 @@ import re
 import time
 from datetime import datetime
 import warnings
+import json
+from glob import glob
 
 
 def delete_file(fp):
@@ -38,7 +40,7 @@ def get_text(DOI:str) -> str:
     """gets the text from a given DOI, will need to add in a database var soon as it will be different"""
     hostname = socket.gethostname()
     path = pathlib.Path(__file__).parent.absolute()
-    name = hostname + DOI.replace("/", "") + ".pdf"
+    name = hostname + str(DOI).replace("/", "") + ".pdf"
     fp = Path(path / "pdfs" / name)  # build filepath
     url = "https://www.medrxiv.org/content/" + DOI + "v1.full.pdf"  # build url
     response = requests.get(url)
@@ -63,80 +65,88 @@ def get_text(DOI:str) -> str:
 
 
 def process_text(row) -> dict:
+    # try:
+    """processes the pdfs and handles matches and returning data"""
+    run = {}
+    text = get_text(str(row["DOI"]))
+    hostname = socket.gethostname()
+    path = pathlib.Path(__file__).parent.absolute()
+    name = hostname + row["DOI"].replace("/", "") + ".pdf"
+    fp = Path(path / "pdfs" / name)  # build filepath
     try:
-        """processes the pdfs and handles matches and returning data"""
-        run = {}
-        text = get_text(row["DOI"])
-        hostname = socket.gethostname()
-        path = pathlib.Path(__file__).parent.absolute()
-        name = hostname + row["DOI"].replace("/", "") + ".pdf"
-        fp = Path(path / "pdfs" / name)  # build filepath
-        try:
-            delete_file(fp)
-        except PermissionError:
-            time.sleep(1)
-            delete_file(fp)
-            pass
-        # get search params TODO::make adaptable
-        search_params = {"R0": ["R0=", "R0 =", "R0", "reproductive number"]}
-        R0_LOWER_BOUND = 0.9
-        R0_UPPER_BOUND = 6.5
-        OFFSET = 15
-        string_matches = []
-        float_matches = []
-        final_matches = []
-        # TODO::functionalize
-        # search each paramater
-        for param in search_params.keys():
-            if param.lower() in text:
-                # search each subparamater
-                for param_type in search_params[param]:
-                    # regex
-                    for param_type_match in re.finditer(param_type.lower(), text):
-                        # grab the string plus the OFFSET (x chars after the param_type was found)
-                        potential_match_string = text[param_type_match.start():param_type_match.end() + OFFSET]
-                        # if param_type_match is not at the end of a sentence, grab it
-                        try:
-                            if potential_match_string[potential_match_string.index(param_type) + len(param_type) + 1] != '.':
-                                string_matches.append(potential_match_string)
-                        except ValueError:
-                            pass
-        # strip all the flaots out
-        float_finder = re.compile(r"[-+]?\d*\.\d+|\d+")
-        for string_match in string_matches:
-                    # appends a list of floats or an empty list
-                    float_matches.append(re.findall(float_finder, string_match))
-        for float_list in float_matches:
-            if len(float_list) > 0:
-                for float_match in float_list:
-                    f = float(float_match)
-                    if f > R0_LOWER_BOUND and f < R0_UPPER_BOUND:
-                        final_matches.append(f)
-        print("-----------------------------------------------------------------------------------------------------------------------")
-        print(row["title"])
-        if len(final_matches) > 0:
-                    final_matches = list(set(final_matches))
-                    print("R0 Found", final_matches)
-                    run["title"] = row["title"]
-                    run["DOI"] = row["DOI"]
-                    run["abstract"] = row["abstract"]
-                    run["pre_print_release_date"] = row["pre_print_release_date"]
-                    run["publisher"] = row["publisher"]
-                    run["authored_by"] = row["authored_by"]
-                    run["R0"] = final_matches
-                    # fill in others...
-                    run["database"] = row["database"]
-                    run["flag"] = ""
-                    # add to shared list
-                    return run
-    except Exception as e:
-        print(e)
+        delete_file(fp)
+    except PermissionError:
+        time.sleep(1)
+        delete_file(fp)
         pass
+    # get search params TODO::make adaptable
+    search_params = {"R0": ["R0=", "R0 =", "R0", "reproductive number"]}
+    R0_LOWER_BOUND = 0.9
+    R0_UPPER_BOUND = 6.5
+    OFFSET = 20
+    string_matches = []
+    float_matches = []
+    final_matches = []
+    # TODO::functionalize
+    # search each paramater
+    for param in search_params.keys():
+        if param.lower() in text:
+            # search each subparamater
+            for param_type in search_params[param]:
+                # regex
+                for param_type_match in re.finditer(param_type.lower(), text):
+                    # grab the string plus the OFFSET (x chars after the param_type was found)
+                    potential_match_string = text[param_type_match.start():param_type_match.end() + OFFSET]
+                    # if param_type_match is not at the end of a sentence, grab it
+                    try:
+                        if potential_match_string[potential_match_string.index(param_type) + len(param_type) + 1] != '.':
+                            string_matches.append(potential_match_string)
+                    except ValueError:
+                        pass
+    # strip all the flaots out
+    float_finder = re.compile(r"[-+]?\d*\.\d+|\d+")
+    for string_match in string_matches:
+                # appends a list of floats or an empty list
+                float_matches.append(re.findall(float_finder, string_match))
+    for float_list in float_matches:
+        if len(float_list) > 0:
+            for float_match in float_list:
+                f = float(float_match)
+                if f > R0_LOWER_BOUND and f < R0_UPPER_BOUND:
+                    final_matches.append(f)
+    print("-----------------------------------------------------------------------------------------------------------------------")
+    print(row["title"])
+    if len(final_matches) > 0:
+                final_matches = list(set(final_matches))
+                print("R0 Found", final_matches)
+                run["title"] = row["title"]
+                run["DOI"] = row["DOI"]
+                run["abstract"] = row["abstract"]
+                run["pre_print_release_date"] = row["pre_print_release_date"]
+                run["publisher"] = row["publisher"]
+                run["authored_by"] = row["authored_by"]
+                run["R0"] = final_matches
+                # fill in others...
+                run["database"] = row["database"]
+                run["flag"] = ""
+                # write to file
+                name = hostname + str(row["DOI"]).replace("/", "") + ".json"
+                with open(Path(path / name), 'w') as f:
+                    json.dump(run, f)
+                    f.close()
+    # except Exception as e:
+    #     print(e)
+    #     pass
 
 
 def main():
     # get path and read input csv
     path = pathlib.Path(__file__).parent.absolute()
+    # pathing for files
+    if not os.path.isdir(Path(path / "pdfs")):
+        os.mkdir(Path(path / "pdfs"))
+    if not os.path.isdir(Path(path / "jsons")):
+        os.mkdir(Path(path / "jsons"))
     df = pd.read_csv(Path(path / "rxivtest.csv"))
     # explicit spawn for unix
     multiprocessing.set_start_method("spawn")
@@ -144,8 +154,13 @@ def main():
     p = Pool(os.cpu_count())
     # make the generator of dataframe
     rows = gen_rows(df)
-    # start map
-    to_df = p.map(process_text, rows)
+    # start map -> generator
+    to_df_gen = p.map(process_text, rows)
+    to_df = []
+    for f in glob(str(Path(path / "jsons*.json"))):
+        to_df.append(json.load(f))
+        f.close()
+        os.remove(f)
     # make df
     df = pd.DataFrame(to_df)
     # save
