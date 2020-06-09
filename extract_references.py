a9fedbcd0bb73def0facdbabe59c77d1ee4afdb0
Jayce Slesar <47452474+jayceslesar@users.noreply.github.com>
2020-06-09 13:25:52 -0400

    test

--- extract_references.py
+++ extract_references.py
@@ -24,4 +24,15 @@ def get_refs_web(url) -> list:
     return extract_references_from_url(url)
 
 
-print(get_refs_web("https://www.medrxiv.org/content/10.1101/2020.05.21.20108621v1.full.pdf"))
+def get_reds_doi(DOI) -> list:
+    hostname = socket.gethostname()
+    path = pathlib.Path(__file__).parent.absolute()
+    name = hostname + str(DOI).replace("/", "") + ".pdf"
+    fp = Path(path / "pdfs" / name)  # build filepath
+    url = "https://www.medrxiv.org/content/" + str(DOI) + "v1.full.pdf"  # build url
+    response = requests.get(url)
+    fp.write_bytes(response.content)  # save .pdf
+    return extract_references_from_file(fp)
+
+
+print(get_refs_doi("10.1101/2020.05.21.20108621"))
