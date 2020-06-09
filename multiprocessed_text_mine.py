b115bb22db318f5febeebed0e1d172fd7717a121
Jayce Slesar <47452474+jayceslesar@users.noreply.github.com>
2020-06-09 15:35:22 -0400

    te

--- multiprocessed_text_mine.py
+++ multiprocessed_text_mine.py
@@ -169,6 +169,7 @@ def process_text(row) -> dict:
     for string_match in string_matches:
         # appends a list of floats or an empty list
         float_matches.append(re.findall(float_finder, string_match)[0])
+    print(float_matches)
     for f in float_matches:
         if "." in f:
             if float(f) > R0_LOWER_BOUND and float(f) < R0_UPPER_BOUND:
