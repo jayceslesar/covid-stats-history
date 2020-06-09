50f7bdb02af0871dce35bfb78246620e802ab23d
Jayce Slesar <47452474+jayceslesar@users.noreply.github.com>
2020-06-09 13:23:54 -0400

    might work

--- extract_references.py
+++ extract_references.py
@@ -1,7 +1,22 @@
+import pandas as pd
+import pathlib
+from pathlib import Path
+import requests
+import sys
+import os
+import socket
+import multiprocessing
+from multiprocessing import Pool
+import re
+import time
+from datetime import datetime
+import warnings
+import json
 from refextract import extract_references_from_file, extract_references_from_url
 
 
-def get_refs(fp) -> list:
+
+def get_refs_file(fp) -> list:
     return extract_references_from_file(fp)
 
 
