d776ceab59e86b3df311551977f565067d7a1299
Jayce Slesar <47452474+jayceslesar@users.noreply.github.com>
2020-06-10 15:38:52 -0400

    r0 good search

--- extract_references.py
+++ extract_references.py
@@ -155,7 +155,7 @@ def find_refs(row):
 
 
 path = pathlib.Path(__file__).parent.absolute()
-df = pd.read_csv(Path(path / "rxiv.csv"))
+df = pd.read_csv(Path(path / "mined.csv"))
 # multiprocessing.set_start_method("spawn")
 # use all CPU's
 p = Pool(os.cpu_count())
